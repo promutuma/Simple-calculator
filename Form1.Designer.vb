876250dcaecbff260004d0387226887ce1a85d63
promutuma <44328078+promutuma@users.noreply.github.com>
2020-11-15 13:03:59 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.cmd0 = New System.Windows.Forms.Button()
+        Me.cmd1 = New System.Windows.Forms.Button()
+        Me.cmd2 = New System.Windows.Forms.Button()
+        Me.cmd3 = New System.Windows.Forms.Button()
+        Me.cmd4 = New System.Windows.Forms.Button()
+        Me.cmdclear = New System.Windows.Forms.Button()
+        Me.cmdequal = New System.Windows.Forms.Button()
+        Me.cmd7 = New System.Windows.Forms.Button()
+        Me.cmd6 = New System.Windows.Forms.Button()
+        Me.cmd5 = New System.Windows.Forms.Button()
+        Me.cmdsin = New System.Windows.Forms.Button()
+        Me.cmdmultiply = New System.Windows.Forms.Button()
+        Me.cmdadd = New System.Windows.Forms.Button()
+        Me.cmd9 = New System.Windows.Forms.Button()
+        Me.cmd8 = New System.Windows.Forms.Button()
+        Me.cmdtan = New System.Windows.Forms.Button()
+        Me.cmdcos = New System.Windows.Forms.Button()
+        Me.cmdsqrt = New System.Windows.Forms.Button()
+        Me.cmdsquare = New System.Windows.Forms.Button()
+        Me.cmdsubtract = New System.Windows.Forms.Button()
+        Me.cmddivide = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'txtBox
+        '
+        Me.txtBox.Location = New System.Drawing.Point(215, 59)
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(348, 23)
+        Me.txtBox.TabIndex = 1
+        '
+        'cmd0
+        '
+        Me.cmd0.Location = New System.Drawing.Point(215, 110)
+        Me.cmd0.Name = "cmd0"
+        Me.cmd0.Size = New System.Drawing.Size(47, 25)
+        Me.cmd0.TabIndex = 2
+        Me.cmd0.Text = "0"
+        Me.cmd0.UseVisualStyleBackColor = True
+        '
+        'cmd1
+        '
+        Me.cmd1.Location = New System.Drawing.Point(268, 110)
+        Me.cmd1.Name = "cmd1"
+        Me.cmd1.Size = New System.Drawing.Size(46, 25)
+        Me.cmd1.TabIndex = 3
+        Me.cmd1.Text = "1"
+        Me.cmd1.UseVisualStyleBackColor = True
+        '
+        'cmd2
+        '
+        Me.cmd2.Location = New System.Drawing.Point(320, 110)
+        Me.cmd2.Name = "cmd2"
+        Me.cmd2.Size = New System.Drawing.Size(46, 25)
+        Me.cmd2.TabIndex = 4
+        Me.cmd2.Text = "2"
+        Me.cmd2.UseVisualStyleBackColor = True
+        '
+        'cmd3
+        '
+        Me.cmd3.Location = New System.Drawing.Point(372, 110)
+        Me.cmd3.Name = "cmd3"
+        Me.cmd3.Size = New System.Drawing.Size(47, 25)
+        Me.cmd3.TabIndex = 5
+        Me.cmd3.Text = "3"
+        Me.cmd3.UseVisualStyleBackColor = True
+        '
+        'cmd4
+        '
+        Me.cmd4.Location = New System.Drawing.Point(425, 110)
+        Me.cmd4.Name = "cmd4"
+        Me.cmd4.Size = New System.Drawing.Size(41, 25)
+        Me.cmd4.TabIndex = 6
+        Me.cmd4.Text = "4"
+        Me.cmd4.UseVisualStyleBackColor = True
+        '
+        'cmdclear
+        '
+        Me.cmdclear.Location = New System.Drawing.Point(520, 110)
+        Me.cmdclear.Name = "cmdclear"
+        Me.cmdclear.Size = New System.Drawing.Size(43, 25)
+        Me.cmdclear.TabIndex = 7
+        Me.cmdclear.Text = "C"
+        Me.cmdclear.UseVisualStyleBackColor = True
+        '
+        'cmdequal
+        '
+        Me.cmdequal.Location = New System.Drawing.Point(472, 110)
+        Me.cmdequal.Name = "cmdequal"
+        Me.cmdequal.Size = New System.Drawing.Size(42, 25)
+        Me.cmdequal.TabIndex = 8
+        Me.cmdequal.Text = "="
+        Me.cmdequal.UseVisualStyleBackColor = True
+        '
+        'cmd7
+        '
+        Me.cmd7.Location = New System.Drawing.Point(320, 155)
+        Me.cmd7.Name = "cmd7"
+        Me.cmd7.Size = New System.Drawing.Size(46, 25)
+        Me.cmd7.TabIndex = 9
+        Me.cmd7.Text = "7"
+        Me.cmd7.UseVisualStyleBackColor = True
+        '
+        'cmd6
+        '
+        Me.cmd6.Location = New System.Drawing.Point(268, 155)
+        Me.cmd6.Name = "cmd6"
+        Me.cmd6.Size = New System.Drawing.Size(46, 25)
+        Me.cmd6.TabIndex = 10
+        Me.cmd6.Text = "6"
+        Me.cmd6.UseVisualStyleBackColor = True
+        '
+        'cmd5
+        '
+        Me.cmd5.Location = New System.Drawing.Point(215, 155)
+        Me.cmd5.Name = "cmd5"
+        Me.cmd5.Size = New System.Drawing.Size(47, 25)
+        Me.cmd5.TabIndex = 11
+        Me.cmd5.Text = "5"
+        Me.cmd5.UseVisualStyleBackColor = True
+        '
+        'cmdsin
+        '
+        Me.cmdsin.Location = New System.Drawing.Point(372, 201)
+        Me.cmdsin.Name = "cmdsin"
+        Me.cmdsin.Size = New System.Drawing.Size(47, 25)
+        Me.cmdsin.TabIndex = 12
+        Me.cmdsin.Text = "Sin"
+        Me.cmdsin.UseVisualStyleBackColor = True
+        '
+        'cmdmultiply
+        '
+        Me.cmdmultiply.Location = New System.Drawing.Point(520, 155)
+        Me.cmdmultiply.Name = "cmdmultiply"
+        Me.cmdmultiply.Size = New System.Drawing.Size(43, 25)
+        Me.cmdmultiply.TabIndex = 13
+        Me.cmdmultiply.Text = "*"
+        Me.cmdmultiply.UseVisualStyleBackColor = True
+        '
+        'cmdadd
+        '
+        Me.cmdadd.Location = New System.Drawing.Point(472, 155)
+        Me.cmdadd.Name = "cmdadd"
+        Me.cmdadd.Size = New System.Drawing.Size(42, 25)
+        Me.cmdadd.TabIndex = 14
+        Me.cmdadd.Text = "+"
+        Me.cmdadd.UseVisualStyleBackColor = True
+        '
+        'cmd9
+        '
+        Me.cmd9.Location = New System.Drawing.Point(425, 155)
+        Me.cmd9.Name = "cmd9"
+        Me.cmd9.Size = New System.Drawing.Size(41, 25)
+        Me.cmd9.TabIndex = 15
+        Me.cmd9.Text = "9"
+        Me.cmd9.UseVisualStyleBackColor = True
+        '
+        'cmd8
+        '
+        Me.cmd8.Location = New System.Drawing.Point(372, 155)
+        Me.cmd8.Name = "cmd8"
+        Me.cmd8.Size = New System.Drawing.Size(47, 25)
+        Me.cmd8.TabIndex = 16
+        Me.cmd8.Text = "8"
+        Me.cmd8.UseVisualStyleBackColor = True
+        '
+        'cmdtan
+        '
+        Me.cmdtan.Location = New System.Drawing.Point(425, 201)
+        Me.cmdtan.Name = "cmdtan"
+        Me.cmdtan.Size = New System.Drawing.Size(41, 25)
+        Me.cmdtan.TabIndex = 17
+        Me.cmdtan.Text = "tan"
+        Me.cmdtan.UseVisualStyleBackColor = True
+        '
+        'cmdcos
+        '
+        Me.cmdcos.Location = New System.Drawing.Point(320, 201)
+        Me.cmdcos.Name = "cmdcos"
+        Me.cmdcos.Size = New System.Drawing.Size(46, 25)
+        Me.cmdcos.TabIndex = 18
+        Me.cmdcos.Text = "Cos"
+        Me.cmdcos.UseVisualStyleBackColor = True
+        '
+        'cmdsqrt
+        '
+        Me.cmdsqrt.Location = New System.Drawing.Point(268, 201)
+        Me.cmdsqrt.Name = "cmdsqrt"
+        Me.cmdsqrt.Size = New System.Drawing.Size(46, 25)
+        Me.cmdsqrt.TabIndex = 19
+        Me.cmdsqrt.Text = "sqrt"
+        Me.cmdsqrt.UseVisualStyleBackColor = True
+        '
+        'cmdsquare
+        '
+        Me.cmdsquare.Location = New System.Drawing.Point(215, 201)
+        Me.cmdsquare.Name = "cmdsquare"
+        Me.cmdsquare.Size = New System.Drawing.Size(47, 25)
+        Me.cmdsquare.TabIndex = 20
+        Me.cmdsquare.Text = "*2"
+        Me.cmdsquare.UseVisualStyleBackColor = True
+        '
+        'cmdsubtract
+        '
+        Me.cmdsubtract.Location = New System.Drawing.Point(472, 201)
+        Me.cmdsubtract.Name = "cmdsubtract"
+        Me.cmdsubtract.Size = New System.Drawing.Size(42, 25)
+        Me.cmdsubtract.TabIndex = 21
+        Me.cmdsubtract.Text = "-"
+        Me.cmdsubtract.UseVisualStyleBackColor = True
+        '
+        'cmddivide
+        '
+        Me.cmddivide.Location = New System.Drawing.Point(520, 201)
+        Me.cmddivide.Name = "cmddivide"
+        Me.cmddivide.Size = New System.Drawing.Size(43, 25)
+        Me.cmddivide.TabIndex = 22
+        Me.cmddivide.Text = "/"
+        Me.cmddivide.UseVisualStyleBackColor = True
+        '
+        'frmCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmddivide)
+        Me.Controls.Add(Me.cmdsubtract)
+        Me.Controls.Add(Me.cmdsquare)
+        Me.Controls.Add(Me.cmdsqrt)
+        Me.Controls.Add(Me.cmdcos)
+        Me.Controls.Add(Me.cmdtan)
+        Me.Controls.Add(Me.cmd8)
+        Me.Controls.Add(Me.cmd9)
+        Me.Controls.Add(Me.cmdadd)
+        Me.Controls.Add(Me.cmdmultiply)
+        Me.Controls.Add(Me.cmdsin)
+        Me.Controls.Add(Me.cmd5)
+        Me.Controls.Add(Me.cmd6)
+        Me.Controls.Add(Me.cmd7)
+        Me.Controls.Add(Me.cmdequal)
+        Me.Controls.Add(Me.cmdclear)
+        Me.Controls.Add(Me.cmd4)
+        Me.Controls.Add(Me.cmd3)
+        Me.Controls.Add(Me.cmd2)
+        Me.Controls.Add(Me.cmd1)
+        Me.Controls.Add(Me.cmd0)
+        Me.Controls.Add(Me.txtBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmCalc"
+        Me.Text = " "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBox As TextBox
+    Friend WithEvents cmd0 As Button
+    Friend WithEvents cmd1 As Button
+    Friend WithEvents cmd2 As Button
+    Friend WithEvents cmd3 As Button
+    Friend WithEvents cmd4 As Button
+    Friend WithEvents cmdclear As Button
+    Friend WithEvents cmdequal As Button
+    Friend WithEvents cmd7 As Button
+    Friend WithEvents cmd6 As Button
+    Friend WithEvents cmd5 As Button
+    Friend WithEvents cmdsin As Button
+    Friend WithEvents cmdmultiply As Button
+    Friend WithEvents cmdadd As Button
+    Friend WithEvents cmd9 As Button
+    Friend WithEvents cmd8 As Button
+    Friend WithEvents cmdtan As Button
+    Friend WithEvents cmdcos As Button
+    Friend WithEvents cmdsqrt As Button
+    Friend WithEvents cmdsquare As Button
+    Friend WithEvents cmdsubtract As Button
+    Friend WithEvents cmddivide As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,156 @@
+﻿Option Explicit On
+Option Strict Off
+
+Public Class frmCalc
+
+
+    Dim sign As String
+    Dim val1 As Double
+    Dim val2 As Double
+
+
+    Private Sub frmCalc_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub cmd0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd0.Click
+        txtBox.Text = txtBox.Text & cmd0.Text 'get 0
+    End Sub
+
+    Private Sub cmd1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd1.Click
+        txtBox.Text = txtBox.Text & cmd1.Text 'get 1
+    End Sub
+
+    Private Sub cmd2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd2.Click
+        txtBox.Text = txtBox.Text & cmd2.Text 'get 2
+    End Sub
+
+    Private Sub cmd3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd3.Click
+        txtBox.Text = txtBox.Text & cmd3.Text 'get 3
+    End Sub
+
+    Private Sub cmd4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd4.Click
+        txtBox.Text = txtBox.Text & cmd4.Text 'get 4
+    End Sub
+
+    Private Sub cmd5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd5.Click
+        txtBox.Text = txtBox.Text & cmd5.Text 'get 5
+    End Sub
+
+    Private Sub cmd6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd6.Click
+        txtBox.Text = txtBox.Text & cmd6.Text
+    End Sub
+
+    Private Sub cmd7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd7.Click
+        txtBox.Text = txtBox.Text & cmd7.Text
+    End Sub
+
+    Private Sub cmd8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd8.Click
+        txtBox.Text = txtBox.Text & cmd8.Text
+    End Sub
+
+    Private Sub cmd9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd9.Click
+        txtBox.Text = txtBox.Text & cmd9.Text
+    End Sub
+
+    Private Sub cmdClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdclear.Click
+        txtBox.Text = ""
+        val1 = 0
+        val2 = 0
+        sign = ""
+    End Sub
+
+    Private Sub cmdCos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdcos.Click
+        Dim v As Double
+        On Error GoTo aa
+        v = CDbl(txtBox.Text)
+        txtBox.Text = Math.Cos(v)
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdDivide_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmddivide.Click
+        sign = "/"
+        On Error GoTo aa
+        val1 = CDbl(txtBox.Text)
+        txtBox.Text = ""
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdEqual_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdequal.Click
+        On Error GoTo aa
+        val2 = CDbl(txtBox.Text)
+        If (sign = "+") Then
+            txtBox.Text = val1 + val2
+        ElseIf (sign = "-") Then
+            txtBox.Text = val1 - val2
+        ElseIf (sign = "*") Then
+            txtBox.Text = val1 * val2
+        Else : txtBox.Text = val1 / val2
+        End If
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdMultiply_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdmultiply.Click
+        sign = "*"
+        On Error GoTo aa
+        val1 = CDbl(txtBox.Text)
+        txtBox.Text = ""
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdPlus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdadd.Click
+        sign = "+"
+        On Error GoTo aa
+        val1 = CDbl(txtBox.Text)
+        txtBox.Text = ""
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdSubtract_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdsubtract.Click
+        sign = "-"
+        On Error GoTo aa
+        val1 = CDbl(txtBox.Text)
+        txtBox.Text = ""
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdSqrt_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdsqrt.Click
+        Dim v As Double
+        On Error GoTo aa
+        v = CDbl(txtBox.Text)
+        txtBox.Text = Math.Sqrt(v)
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdSin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdsin.Click
+        Dim v As Double
+        On Error GoTo aa
+        v = CDbl(txtBox.Text)
+        txtBox.Text = Math.Sin(v)
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdTan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdtan.Click
+        Dim v As Double
+        On Error GoTo aa
+        v = CDbl(txtBox.Text)
+        txtBox.Text = Math.Tan(v)
+aa:     Exit Sub
+    End Sub
+
+    Private Sub cmdSquare_Click(sender As Object, e As EventArgs) Handles cmdsquare.Click
+        Dim v As Double
+        On Error GoTo aa
+        v = CDbl(txtBox.Text)
+        txtBox.Text = v ^ 2
+aa:     Exit Sub
+    End Sub
+
+    Private Sub txtBox_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtBox.KeyPress
+        If Asc(e.KeyChar) >= Asc("0") And Asc(e.KeyChar) <= Asc("9") Or Asc(e.KeyChar) = 8 Or Asc(e.KeyChar) = 46 Then
+            Exit Sub
+        Else
+            e.KeyChar = ""
+        End If
+    End Sub
+End Class
